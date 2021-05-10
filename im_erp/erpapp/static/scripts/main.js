99a234a7d3eaada0b3ced5dee9280859bdc6a5bf
igor.matvienko <igor.matvienko@stud.uni-goettingen.de>
2021-05-10 18:53:19 +0200

    confirmation popups for delete, static files

---
+++ im_erp/erpapp/static/scripts/main.js
@@ -0,0 +1,11 @@
+function confirm_delete(fullname){
+var result = confirm("Are you sure you want to delete " + fullname + "?");
+
+
+if (result == true){
+console.log("wow: " + result);
+} else {
+console.log("Hello world! " + result);
+return false;
+}
+}
